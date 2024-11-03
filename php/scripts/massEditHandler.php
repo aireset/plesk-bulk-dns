116a825e778fe5383994a0d38897b14cdaf632ed
Felipe <4226997+zhiru@users.noreply.github.com>
2024-11-03 13:31:36 +0000

    ajustes para testar o funcionamento

--- php/scripts/massEditHandler.php
+++ php/scripts/massEditHandler.php
@@ -1,8 +1,26 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
 use Plesk\Domain;
 use Plesk\DnsRecord;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Processa os registros selecionados e salva as alterações
+    $names = $_POST['name'];
+    $types = $_POST['type'];
+    $contents = $_POST['content'];
+    $ttls = $_POST['ttl'];
+
+    foreach ($names as $index => $name) {
+        $dnsRecord = new DnsRecord();
+        $dnsRecord->setName($name);
+        $dnsRecord->setType($types[$index]);
+        $dnsRecord->setContent($contents[$index]);
+        $dnsRecord->setTtl($ttls[$index]);
+        // Salvar o registro atualizado no Plesk
+        $dnsRecord->save();
+    }
+
+    echo "Registros DNS atualizados com sucesso!";
 }
+?>
