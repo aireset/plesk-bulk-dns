5d1b0efe8775d1235c32da6afdb03e0f4b8ff4e8
Felipe <4226997+zhiru@users.noreply.github.com>
2024-11-03 14:29:50 +0000

    melhorias para teste

--- php/scripts/massEditHandler.php
+++ php/scripts/massEditHandler.php
@@ -1,26 +1,26 @@
 <?php
 
-require_once 'vendor/autoload.php';
-
-use Plesk\Domain;
 use Plesk\DnsRecord;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $names = $_POST['name'];
-    $types = $_POST['type'];
-    $contents = $_POST['content'];
-    $ttls = $_POST['ttl'];
+    $recordIds = $_POST['record_ids'];
+    $newContent = $_POST['content'] ?? [];
+    $newTtl = $_POST['ttl'] ?? [];
 
-    foreach ($names as $index => $name) {
-        $dnsRecord = new DnsRecord();
-        $dnsRecord->setName($name);
-        $dnsRecord->setType($types[$index]);
-        $dnsRecord->setContent($contents[$index]);
-        $dnsRecord->setTtl($ttls[$index]);
-        // Salvar o registro atualizado no Plesk
-        $dnsRecord->save();
+    foreach ($recordIds as $type => $ids) {
+        $content = $newContent[$type] ?? null;
+        $ttl = $newTtl[$type] ?? null;
+
+        foreach ($ids as $recordId) {
+            $dnsRecord = DnsRecord::getById($recordId);
+            if ($dnsRecord && $dnsRecord->getType() === $type) {
+                if ($content) $dnsRecord->setContent($content);
+                if ($ttl) $dnsRecord->setTtl($ttl);
+                $dnsRecord->save();
+            }
+        }
     }
 
-    echo "Registros DNS atualizados com sucesso!";
+    echo "Valores e TTLs atualizados com sucesso!";
 }
 ?>
--- php/scripts/toolbar.php
+++ php/scripts/toolbar.php
@@ -4,12 +4,21 @@ use Plesk\Components\Toolbar;
 use pm_View_List_Simple;
 
 $toolbar = Toolbar::getInstance();
+
+// Botão para edição em massa de valores e TTL
 $toolbar->addButton([
-    'text' => 'Editar em Massa',
-    'url' => pm_Context::getActionUrl('plesk-bulk-dns', 'edit'),
+    'text' => 'Editar Valores e TTL',
+    'url' => pm_Context::getActionUrl('plesk-bulk-dns', 'edit_mass_values'),
     'place' => pm_View_List_Simple::PLACE_TOOLBAR,
     'context' => 'dns-list',
     'class' => 'btn',
 ]);
 
-?>
+// Botão para edição completa (Nome, Tipo, Conteúdo e TTL)
+$toolbar->addButton([
+    'text' => 'Edição Completa em Massa',
+    'url' => pm_Context::getActionUrl('plesk-bulk-dns', 'edit_mass_dns'),
+    'place' => pm_View_List_Simple::PLACE_TOOLBAR,
+    'context' => 'dns-list',
+    'class' => 'btn',
+]);
